79dcc51e671eaa99cd78c14558c4414de43b57bc
leesh <lsh80165@gmail.com>
2022-04-27 21:56:55 +0900

    project_create_and_git_remote_add

---
+++ src/main/java/miniProject/miniwebProject/MiniWebProjectApplication.java
@@ -0,0 +1,13 @@
+package miniProject.miniwebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWebProjectApplication.class, args);
+	}
+
+}
